fae86547136afbb5163feb02db12534aa9397b33
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-08-20 19:58:51 +0400

    asdasadasd

--- FibonacciIterator/FibonacciEnumerable.cs
+++ FibonacciIterator/FibonacciEnumerable.cs
@@ -1,14 +1,30 @@
-﻿namespace FibonacciIterator
+﻿using System.Collections;
+
+namespace FibonacciIterator
 {
     /// <summary>
     /// Represents an enumerable object to iterate over the Fibonacci sequence numbers.
     /// </summary>
-    public sealed class FibonacciEnumerable
+    public sealed class FibonacciEnumerable : IEnumerable<int>
     {
         // TODO Implement an enumerable to iterate over the Fibonacci sequence numbers.
+        private int count;
+        private int skipCount;
 
         public FibonacciEnumerable(int count = int.MaxValue, int skipCount = 0)
         {
+            this.count = count;
+            this.skipCount = skipCount;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new FibonacciEnumerator(this.count, this.skipCount);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 }
--- FibonacciIterator/FibonacciEnumerator.cs
+++ FibonacciIterator/FibonacciEnumerator.cs
@@ -1,16 +1,67 @@
-﻿namespace FibonacciIterator
+﻿using System.Collections;
+
+namespace FibonacciIterator
 {
     /// <summary>
     /// Represents an enumerator object to iterate over the Fibonacci sequence numbers.
     /// </summary>
-    public sealed class FibonacciEnumerator
+    public sealed class FibonacciEnumerator : IEnumerator, IEnumerator<int>
     {
         // TODO Implement an enumerator to iterate over the Fibonacci sequence numbers.
+        private int count;
+        private int skipCount;
+        private int currentFib;
+        private int previousFib;
+        private int fibIndex;
+
+        public FibonacciEnumerator(int count, int skipCount)
+        {
+            this.count = count;
+            this.skipCount = skipCount;
+
+            this.currentFib = 0;
+            this.previousFib = 1;
+            this.fibIndex = 0;
+
+            // Skipping the specified number of elements
+            for (int i = 0; i < skipCount; i++)
+            {
+                this.NextFibonacci();
+            }
+        }
+
+        public int Current => this.currentFib;
+
+        // ReSharper disable once HeapView.BoxingAllocation
+        object IEnumerator.Current => this.Current;
 
         public void Dispose()
         {
-            // The method is empty, because there are no additional resources to dispose.
-            // See "Notes to Implementers" - https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ienumerator-1?view=net-6.0#notes-to-implementers
+            // No resources to dispose
+        }
+
+        public bool MoveNext()
+        {
+            if (this.fibIndex < this.count)
+            {
+                this.NextFibonacci();
+                this.fibIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            throw new NotSupportedException();
+        }
+
+        private void NextFibonacci()
+        {
+            int temp = this.currentFib;
+            this.currentFib += this.previousFib;
+            this.previousFib = temp;
         }
     }
 }
